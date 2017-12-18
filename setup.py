5d2355e15b0373fa6fc5495fa53cbecf52fe50d4
Corey <corey.r.girard@gmail.com>
2017-12-18 00:20:04 -0500

    Beginning rebranding and PyPI deploy

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lifx.py',
+      version='0.1',
+      description='Talks to the LIFX API',
+      url='http://github.com/crgirard/lifxpy',
+      author='Corey Girard',
+      author_email='corey.r.girard@gmail.com',
+      license='MIT',
+      packages=['lifxpy'],
+      zip_safe=False)
